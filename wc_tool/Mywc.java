4b1dda8014971bfaf2bab10e82229fe3b8830828
kunalsjaswal <kunal23singh0@gmail.com>
2026-02-24 19:25:37 +0530

    wc tool: readBytes feature

---
+++ wc_tool/Mywc.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Arrays;
+
+class Mywc {
+  private static void countBytes(File file, FileInputStream fin, String filePath) {
+    try {
+      System.out.println(file.length() + " " + filePath);
+    } catch (Exception e) {
+      System.out.println("Error decoding bytes: " + e.getMessage());
+    }
+  }
+
+  private static void countLines(File file, FileInputStream fin, String filePath) {
+    try {
+      
+    } catch (Exception e) {
+      System.out.println("Error decoding lines: " + e.getMessage());
+    }
+  }
+
+  public static void main(String[] args) {
+    if(args.length < 2) {
+      System.out.println("Please enter arguments");
+      return;
+    }
+
+
+    String command = args[0];
+    String filePath = args[1];
+
+    try (FileInputStream fin = new FileInputStream(filePath)){
+      File file = new File(filePath); // cannot close file
+
+      if(command.equals("-c")) {
+        countBytes(file, fin, filePath); // output: 342190 test.txt
+      } else if(command.equals("-l")) {
+        countLines(file, fin, filePath);
+      }
+
+    } catch (Exception e) {
+      System.out.println("Error reading file.." + e.getMessage());
+    }
+  }
+}
